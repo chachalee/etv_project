6969abdb936931ced369f49612be2dcc93c0f55c
Edith Li <abenoy05@gmail.com>
2017-02-09 23:41:09 -0500

    no texture

---
+++ Assets/Script/elevatorLift.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class elevatorLift : MonoBehaviour {
+
+	private bool pressedButton = false;
+	private bool isElevatorUp = false;
+
+	GameObject target;
+
+	void OnMouseOver()
+	{
+		pressedButton = true;
+	}
+
+	void OnMouseExit()
+	{
+		pressedButton = false;
+	}
+
+	void OnMouseDown()
+	{
+		if(isElevatorUp == false)
+		{
+			target = GameObject.Find("Elevator");
+			target.GetComponent<Animation>().Play("1");
+			Debug.Log ("Up");
+			isElevatorUp = true;
+		}
+		else
+		{
+			target = GameObject.Find("Elevator");
+			target.GetComponent<Animation>().Play("2");
+			Debug.Log ("Down");
+			isElevatorUp = false;
+		}
+	}
+ 
+}
